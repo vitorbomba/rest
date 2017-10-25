1c3e611900fb3774dcbd77988de63a5e1de8698b
vitorbomba <vitor.bomba@terra.com.br>
2017-10-25 19:41:35 -0200

    rest 25.10

---
+++ js/animacao.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+	$(".animar-interno1").addClass("animated fadeInLeft");
+}, 1200);
+setTimeout(function() {
+	$(".animar-interno2").addClass("animated fadeInLeft");
+}, 1500)
+setTimeout(function() {
+	$(".animar-interno3").addClass("animated fadeInLeft");
+}, 1800)
